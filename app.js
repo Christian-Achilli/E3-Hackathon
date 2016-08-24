1292644d895a6811685e1bbc285c86e5b88de4c6
Christian Achilli <chrisachilli@gmail.com>
2016-08-24 23:07:55 +0100

    connected add app form

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var addAppsForm = require('./routes/addform');
+
+var neodao = require('./dao/neo4jdao');
+
+var fs = require("fs");
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +27,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/addAppForm', addAppsForm);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
