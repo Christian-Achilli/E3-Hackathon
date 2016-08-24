ebe887b8d8b4498a6b35d9880dbe105ddef2ac89
Oliver Bouchard <oliver@enobo.com>
2016-08-24 18:06:44 -0400

    added API for adding new system

--- server/appapi.js
+++ server/appapi.js
@@ -1,12 +1,36 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+//app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 var db = require("../dao/neo4jdao");
 
+// List all apps
 app.get('/listapps', function (req, res) {
 	db.cypherQuery(
 		    'MATCH (systems) RETURN systems LIMIT 50',
 		    {}
 		     , function (err, result) {
+		        if (err) {
+		            return console.log(err);
+		        }
+		        console.log(result.data); // delivers an array of query results
+		        console.log(result.columns); // delivers an array of names of objects getting returned
+		        
+		        res.end(JSON.stringify(result.data));
+		    }
+		);
+})
+
+// add new connection
+app.post('/addapp', function (req, res) {
+	console.log(req.body);
+	db.cypherQuery(
+		    'MATCH (s:System {csi : { from_csi }}) ' +
+		    'CREATE (s)-[:CONNECT]->(t:System {csi:{ to_csi }, name:{ name }}) ' +
+		    'RETURN s, t',
+		    req.body,
+		    function (err, result) {
 		        if (err) {
 		            return console.log(err);
 		        }
